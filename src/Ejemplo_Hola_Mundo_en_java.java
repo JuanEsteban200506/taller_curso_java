a57e12b3737dd59c5a69dfff25bc3e741908b973
Juan Esteban Florez Perlaza <esteban200506@gmail.com>
2022-06-26 21:55:08 -0500

    se inician los trabajos del curso de java

---
+++ src/Ejemplo_Hola_Mundo_en_java.java
@@ -0,0 +1,5 @@
+public class Ejemplo_Hola_Mundo_en_java {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ src/Introduccion_a_las_variables.java
@@ -0,0 +1,5 @@
+public class Introduccion_a_las_variables {
+    public static void main(String[] args) {
+
+    }
+}
